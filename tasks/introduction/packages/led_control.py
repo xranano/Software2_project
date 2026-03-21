5b0db54c787a059d8694e04644cb48de62ed3f06
Anano Kuchava <Kuchava.Anano@kiu.edu.ge>
2026-03-22 00:15:12 +0400

    my local changes

--- tasks/introduction/packages/led_control.py
+++ tasks/introduction/packages/led_control.py
@@ -1,7 +1,43 @@
 import colorsys
 from typing import List
 
-
 def set_turning_leds(direction: str) -> dict:
-    """Set LEDs to indicate turning direction."""
-    raise NotImplementedError("TODO: Implement this function")
+    yellow = [1.0, 1.0, 0.0]
+    white = [1.0, 1.0, 1.0]
+    red = [1.0, 0.0, 0.0]
+    off = [0.0, 0.0, 0.0]
+    if direction == "left":
+        return {
+            0: yellow,
+            2: off,
+            3: off,
+            4: yellow
+        }
+    elif direction == "right":
+        return {
+            0: off,
+            2: yellow,
+            3: yellow,
+            4: off
+        }
+    elif direction == "forward":
+        return {
+            0: white,
+            2: white,
+            3: off,
+            4: off
+        }
+    elif direction == "stop":
+        return {
+            0: off,
+            2: off,
+            3: red,
+            4: red
+        }
+    else:
+        return {
+            0: off,
+            2: off,
+            3: off,
+            4: off
+        }
